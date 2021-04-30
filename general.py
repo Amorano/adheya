cd0f25da159d590e863c9229f55e0ff41a7ce202
Alexander G. Morano <amorano@gmail.com>
2021-04-29 18:08:09 -0700

    threaded progress bar

--- general.py
+++ general.py
@@ -1,6 +1,8 @@
 """."""
 
 import os
+from queue import Queue
+from threading import Thread
 from dearpygui import core, simple
 from adheya import DPGObject
 
@@ -80,9 +82,9 @@ class FileImage(FileHandle):
 		core.add_drawing(self.__idCanvas, width=self.__max[0], height=self.__max[1], parent=self.parent.guid)
 		self.__data = []
 		self.register('zoom', self.__zoom)
-		self.__zoom(zoomLevel)
+		self.__zoom(self, zoomLevel)
 
-	def __zoom(self, level):
+	def __zoom(self, sender, level):
 		size = pow(2, 5 + level)
 		self.__max = (size, size)
 		core.configure_item(self.__idCanvas, width=size, height=size)
@@ -92,3 +94,71 @@ class FileImage(FileHandle):
 		"""Callback when file selector picks new file."""
 		if self.path:
 			core.draw_image(self.__idCanvas, self.path, pmin=self.__min, pmax=self.__max)
+
+class ThreadProgress(Thread):
+	"""Threaded progress bar widget."""
+	def __init__(self, progressBar, queue, callback=None):
+		super().__init__()
+		self.daemon = True
+		self.__q = queue
+		self.__progressBar = progressBar
+		self.__callback = callback
+
+	def run(self):
+		while self.is_alive:
+			val, overlay = self.__q.get()
+			if val is None:
+				break
+			core.configure_item(self.__progressBar.guid, overlay=overlay)
+			core.set_value(self.__progressBar.guid, val)
+
+		if self.__callback:
+			self.__callback()
+
+class ProgressBar():
+	_index = 0
+	def __init__(self, parent, guid=None, callback=None, **kw):
+
+		guid = guid or self.__class__.__name__
+		self.__guid = f'{parent}-{guid}.{ProgressBar._index}'
+		self.__idGroup = f"{self.__guid}-group"
+		ProgressBar._index += 1
+
+		self.__callback = callback
+		self.__worker = None
+		self.__q = Queue()
+		self.__value = 0
+
+		width, _ = core.get_item_rect_size(parent)
+		width = int(width)
+		kw['width'] = kw.get('width', width)
+		kw['show'] = kw.get('show', False)
+		kw.pop('parent', None)
+		with simple.group(self.__idGroup, width=width, parent=parent):
+			core.add_progress_bar(self.__guid, **kw)
+
+	@property
+	def progress(self):
+		return self.__value
+
+	@progress.setter
+	def progress(self, val):
+		self.__value = val
+		data = f'{round(val * 100)}%'
+		if val < 0:
+			val = 0
+		if val > 1.:
+			val = None
+		self.__q.put((val, data))
+
+	@property
+	def guid(self):
+		return self.__guid
+
+	def start(self):
+		simple.show_item(self.__guid)
+		self.__worker = ThreadProgress(self, self.__q, callback=lambda: self.__callback(self.__guid))
+		self.__worker.start()
+
+	def reset(self):
+		self.__value = 0
